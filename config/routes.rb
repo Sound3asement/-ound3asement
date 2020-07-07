949265de0b579411d3dca59f5e8fc2470eadca7a
$onny <hgpmac157@Sonnys-Air.hsd1.ca.comcast.net>
2020-07-07 13:51:52 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  # get '/budPage', to: 'home#Bud'
+  # get '/cartsPage', to: 'home#Carts'
+  # get '/extractsPage', to: 'home#Extracts'
+  # get '/glassPage', to: 'home#Glass'
+  # get '/THCvsCBD', to: 'home#THC_CBD'
+end
