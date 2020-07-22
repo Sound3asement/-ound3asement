6b4bd0291435d8900b93f733e486d7b86cb45363
$onny <hgpmac157@Sonnys-Air.hsd1.ca.comcast.net>
2020-07-22 13:42:28 -0700

    update jul 22

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+def index
+end
+
+def new 
+end
+
+def create
+end
+
+def show
+end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_07_02_204119) do
 
   create_table "users", force: :cascade do |t|
+    t.string "fname", default: ""
+    t.string "lname", default: ""
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
