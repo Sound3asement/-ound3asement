21f55dd8d5eb951623122717af76bd254a9a6f16
$onny <hgpmac157@Sonnys-Air.hsd1.ca.comcast.net>
2020-07-23 21:18:18 -0700

    update jul 23

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,23 @@ def new
 end
 
 def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
 end
 
 def show
+    @post = Post.find(params[:id])
 end
 
+private
+
+    def post_params
+    params.require(:post).permit(:title, :body)
+    end
 end
+
+
+
+
